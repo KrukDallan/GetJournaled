3977ae0a0e353d3393152564463b3ab4e171b1b2
KrukDallan <99502048+KrukDallan@users.noreply.github.com>
2023-12-10 10:28:50 +0100

    Started experimenting with the drawer

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,46 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:getjournaled/shared.dart';
 
-class Drawer extends StatefulWidget{
-    @override
+class Drawer extends StatefulWidget {
+  @override
   State<Drawer> createState() => _Drawer();
 }
 
-class _Drawer extends State<Drawer>{
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(0, 0, 0, 0),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(padding: topPadding),
-              Center(child:Text('Your drawer')),
-            ],
-          ),
-        ),
-      );
-  }
-}
-
-class DrawerPage extends StatelessWidget{
+class _Drawer extends State<Drawer> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        decoration: BoxDecoration(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(padding: customTopPadding(0.1)),
+              const Text('Your drawer'),
+              Padding(padding: customTopPadding(0.1)),
+              const SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 280,
+                            height: 200,
+                            child: Center(child: Text('sample')),
+                            ),
+                        ],
+                      ),
+                  ),
+                  Card(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 280,
+                            height: 200,
+                            child: Center(child: Text('sample')),
+                            ),
+                        ],
+                      ),
+                  ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerPage extends StatelessWidget {
+  const DrawerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+      decoration: BoxDecoration(
           gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
             Colors.blue[200]!,
             Colors.purple[200]!,
-          ]
-        )
-        ),
-        child: Drawer(),
-      ));
+          ])),
+      child: Drawer(),
+    ));
   }
-
 }
--- lib/shared.dart
+++ lib/shared.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 const EdgeInsets topPadding = EdgeInsets.only(top: 800*0.5*0.75);
 
-EdgeInsets cstmTopPadding(double factor){
+EdgeInsets customTopPadding(double factor){
   return EdgeInsets.only(top: 800*0.5*factor);
+}
+
+EdgeInsets customBottomPadding(double factor){
+  return EdgeInsets.only(bottom: 800*0.5*factor);
 }
