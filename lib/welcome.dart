336c89c527df6ee5a385e02318cab60269fb94e7
KrukDallan <99502048+KrukDallan@users.noreply.github.com>
2023-12-11 21:17:49 +0100

    added a new button + changed text size

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -23,23 +23,23 @@ class WelcomePage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Padding(padding: customTopPadding(0.25)),
+          Padding(padding: customTopPadding(0.1)),
           Center(
             child: Text(
               'Welcome!',
               style: TextStyle(
                 color: colorScheme.onPrimary,
-                fontSize: 30,
+                fontSize: 45,
                 fontFamily: 'Lobster',
               ),
             ),
           ),
-          Padding(padding: customTopPadding(0.1)),
+          Padding(padding: customTopPadding(0.4)),
           Container(
           height: 50,
-          width: 150,
+          width: 200,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
             gradient: LinearGradient(
               colors: [
             Colors.blue.shade100,
@@ -56,6 +56,35 @@ class WelcomePage extends StatelessWidget {
             onPressed: () {}, 
             child: Text(
                   'New journal',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: colorScheme.onPrimary,
+                  ),
+                ),
+              ),
+      ),
+      Padding(padding: customTopPadding(0.05)),
+          Container(
+          height: 50,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            gradient: LinearGradient(
+              colors: [
+            Colors.blue.shade100,
+            Colors.purple.shade100,
+              ],
+              begin: Alignment.centerRight,
+              end: Alignment.centerLeft,
+            ),
+          ),
+          child: TextButton(
+            style: ButtonStyle(
+              fixedSize: MaterialStateProperty.resolveWith((states) => const Size(180, 50)),
+            ),
+            onPressed: () {}, 
+            child: Text(
+                  'New note',
                   style: TextStyle(
                     fontSize: 18,
                     color: colorScheme.onPrimary,
